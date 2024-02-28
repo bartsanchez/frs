49e40c4537cc5b3bcb3bc74ab1e510b7ed0b44eb
Bartolomé Sánchez Salado <bsanchezsalado@gmail.com>
2024-02-28 12:22:38 +0100

    REF: Update code format using Ruff

--- frs/frs/wsgi.py
+++ frs/frs/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'frs.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "frs.settings")
 
 application = get_wsgi_application()
